5c9c4fbc617a800cb28c8655fc5c84284ffb0a15
dipankarchettri <71317043+dipankarchettri@users.noreply.github.com>
2021-11-21 16:45:53 +0530

    burger

---
+++ sudukoSolver.py
@@ -0,0 +1,98 @@
+
+def backtrack (bo): #implementing the backtrack algorithm 
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if backtrack (bo):
+                return True
+
+            bo[row][col] = 0
+        
+
+    return False
+
+
+def valid(bo, num, pos): # check if the bard is valid or not 
+    # Check  in the row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check in the  column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check in the  box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+def print_board(bo): # printing the board 
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+def find_empty(bo): # finding the empty positions (in this case "zeros")
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+def if_solved(bo):
+    solvable=True
+    for i in range (len(bo)):
+        for j in range (len(bo[0])):
+            if (bo[i][j])==0:
+                solvable=False
+    if not solvable:
+        print("The board is not solvable")
+
+#___main___
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,5],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]# the board to be solved 
+]
+
+print("The unsolved board")
+print()
+print_board(board)
+backtrack (board)
+print("----------------------------------------------------")
+print("----------------------------------------------------")
+print("The solved board")
+print_board(board)
+if_solved(board)
